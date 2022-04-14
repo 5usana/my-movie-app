ae6b4f85683bf8545dac632a5462e21ce64a66a6
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-13 17:52:48 -0700

    started scss styling, added movie poster image to file

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,16 @@
+import poster from '../photos/AvengersPoster.jpg';
+
 export default function MovieDetails({ props }) {
 	return (
 		<div className='MovieDetailsContainer'>
-			<div className='MovieTitle'>{props.Title}</div>
-			<div className='MovieGenre'>{props.Genre}</div>
-			<div className='MoviePoster'>{props.Poster}</div>
-			<div className='MovieRated'>{props.Rated}</div>
+			<div className='MovieCard'>
+				<div className='MovieTitle'>{props.Title}</div>
+				<div className='MovieGenre'>{props.Genre}</div>
+				{/* <div className='MoviePoster'>{props.Poster}</div> */}
+				<img className='MoviePoster' src={poster} />
+
+				<div className='MovieRated'>{props.Rated}</div>
+			</div>
 		</div>
 	);
 }
