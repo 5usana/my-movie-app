64d568833f441020c2e626902befcdd53579b41d
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-13 18:13:39 -0700

    font styling updates

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -5,10 +5,10 @@ export default function MovieDetails({ props }) {
 		<div className='MovieDetailsContainer'>
 			<div className='MovieCard'>
 				<div className='MovieTitle'>{props.Title}</div>
+				<div className='ImdbRating'>{props.imdbRating}</div>
 				<div className='MovieGenre'>{props.Genre}</div>
 				{/* <div className='MoviePoster'>{props.Poster}</div> */}
 				<img className='MoviePoster' src={poster} />
-
 				<div className='MovieRated'>{props.Rated}</div>
 			</div>
 		</div>
