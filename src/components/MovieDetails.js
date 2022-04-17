c5871dced69f78a7c95b9d06a25b5cab603a0bbe
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-17 14:45:23 -0700

    completed hw day 2, MovieDetails takes props for postuerURL, title, rating, runtime, genre, plot, actors, and rated. css needs improving,& bonus work needed

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -7,9 +7,14 @@ export default function MovieDetails({ props }) {
 				<div className='MovieTitle'>{props.Title}</div>
 				<div className='ImdbRating'>{props.imdbRating}</div>
 				<div className='MovieGenre'>{props.Genre}</div>
-				{/* <div className='MoviePoster'>{props.Poster}</div> */}
-				<img className='MoviePoster' src={poster} />
 				<div className='MovieRated'>{props.Rated}</div>
+				{/* <div className='MoviePoster'>{props.Poster}</div> */}
+				<img className='MoviePoster' src={poster} alt='Avengers move poster' />
+				<div className='Runtime'>{props.Runtime}</div>
+				<div className='list'>Actors</div>
+				<div className='Actors'>{props.Actors}</div>
+				<div className='list'>Plot</div>
+				<div className='Plot'>{props.Plot}</div>
 			</div>
 		</div>
 	);
