ce95f34db4e9defd29c6a91dd068fbc24b4458af
Susana Martins <susanamartins@Susanas-MacBook-Pro.local>
2022-04-18 12:11:56 -0700

    day 2 hw cleaned up, api removed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@
 import './App.scss';
 import MovieDetails from './components/MovieDetails';
 import { movieData } from './components/movieData';
+import React from 'react';
+// import { getMoviesByName } from './utils';
 
 function App() {
+	// const [movie, setMovie] = React.useState('');
+
+	// React.useEffect(() => {
+	// 	getMoviesByName = async (name) => {
+	// 		let apiKey = 'API KEY HERE';
+	// 		// accessing the API
+	// 		const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${name}`;
+	// 		// Get an object of detailed information about the specified movie
+	// 		const response = await fetch(url);
+	// 		// return an array of movies with a matching title from OMDb API
+	// 		const data = await response.json();
+	// 		console.log(data);
+	// 		setMovie(data);
+	// 	};
+	// 	getMoviesByName('batman');
+	// }, []);
+
 	return (
 		<div>
 			<MovieDetails props={movieData} />
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,10 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
-export default function MovieList() {
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url);
-				const json = await response.json();
-				console.log(json);
-			} catch (error) {
-				console.log('error', error);
-			}
-		};
-
-		fetchData();
-	}, []);
-
-	return <div className='MovieListContainer'></div>;
+export default function MovieList(props) {
+	return (
+		<div>
+			<MovieCard />
+			<MovieCard />
+		</div>
+	);
 }
